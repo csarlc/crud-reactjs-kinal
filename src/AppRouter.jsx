e2c4d21872844635348c8cd8f6c85bc3766c7498
csarlc <cesarlopez@kinal.edu.gt>
2023-04-13 15:46:43 -0600

    push

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,12 +1,22 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { UserTable } from "./user/components/UserTable";
 import { Login } from "./login/components/Login";
+import { isUserAuthenticated } from "./login/helpers/LoginHelper";
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<UserTable />}></Route>
+        <Route
+          path="/"
+          element={
+            isUserAuthenticated() ? (
+              <UserTable />
+            ) : (
+              <Navigate to="/login"></Navigate>
+            )
+          }
+        ></Route>
         <Route path="/login" element={<Login></Login>}></Route>
       </Routes>
     </>
